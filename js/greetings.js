0033210f7a667ba2e8e70b1de583eb51e98333d5
klkk111 <jkklk877@gmail.com>
2023-02-05 22:55:47 +0900

    second commit

--- js/greetings.js
+++ js/greetings.js
@@ -1,7 +1,6 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -9,7 +8,8 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   painGreetings(username);
 }
 
